fa6df59c04592f81861f913d3bc7531fd25cb6fe
sparky300 <cfsparky@gmail.com>
2013-09-26 10:41:56 +0100

    added sandbox

--- makefile
+++ makefile
@@ -7,11 +7,13 @@ upgrade:
 	python setup.py develop --upgrade
 
 sandbox: install
-	-rm -f sandbox/db.sqlite
-	sandbox/manage.py syncdb --noinput
-	sandbox/manage.py migrate
-	sandbox/manage.py oscar_import_catalogue sandbox/fixtures/*.csv
-	sandbox/manage.py oscar_import_catalogue_images sandbox/fixtures/images.tar.gz
-	sandbox/manage.py loaddata countries.json _fixtures/pages.json _fixtures/auth.json _fixtures/ranges.json _fixtures/offers.json
-	sandbox/manage.py clear_index --noinput
-	sandbox/manage.py update_index catalogue
+    -rm -f sandbox/db.sqlite
+    # Create database
+    sandbox/manage.py syncdb --noinput
+    sandbox/manage.py migrate
+    # Import some fixtures
+    sandbox/manage.py oscar_import_catalogue sandbox/fixtures/*.csv
+    sandbox/manage.py oscar_import_catalogue_images sandbox/fixtures/images.tar.gz
+    sandbox/manage.py loaddata countries.json _fixtures/pages.json _fixtures/auth.json _fixtures/ranges.json _fixtures/offers.json
+    sandbox/manage.py clear_index --noinput
+    sandbox/manage.py update_index catalogue
--- sandbox/urls.py
+++ sandbox/urls.py
@@ -1,13 +1,38 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
+
+from oscar.app import shop
+
+# These simply need to be imported into this namespace.  Ignore the PEP8
+# warning that they aren't used.
+from oscar.views import handler500, handler404, handler403
+
 admin.autodiscover()
 
-
-from django.conf.urls import patterns, include, url
-from oscar.app import application
-
 urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)), #admin site
-    (r'^pages/', include('django.contrib.flatpages.urls')),
-    (r'', include(application.urls)),
+    # Include admin as convenience. It's unsupported and you should
+    # use the dashboard
+    (r'^admin/', include(admin.site.urls)),
+    # Custom functionality to allow dashboard users to be created
+    (r'^gateway/', include('apps.gateway.urls')),
+    (r'', include(shop.urls)),
 )
+
+# Allow rosetta to be used to add translations
+if 'rosetta' in settings.INSTALLED_APPS:
+    urlpatterns += patterns('',
+        (r'^rosetta/', include('rosetta.urls')),
+    )
+
+if settings.DEBUG:
+    # Server statics and uploaded media
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    # Allow error pages to be tested
+    urlpatterns += patterns('',
+        url(r'^403$', handler403),
+        url(r'^404$', handler404),
+        url(r'^500$', handler500)
+    )
